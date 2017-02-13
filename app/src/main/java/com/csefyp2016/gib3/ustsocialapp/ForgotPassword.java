dff4dd07295fd1c934b5b93865dfbc2c420e7539
Christine <pywongak@connect.ust.hk>
2017-02-14 03:08:08 +0800

    Forgot Password.java updated

--- app/src/main/java/com/csefyp2016/gib3/ustsocialapp/ForgotPassword.java
+++ app/src/main/java/com/csefyp2016/gib3/ustsocialapp/ForgotPassword.java
@@ -1,11 +1,13 @@
 package com.csefyp2016.gib3.ustsocialapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ForgotPassword extends AppCompatActivity {
     private static final String URL = "";
