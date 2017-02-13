d73796eb926564519f423dc08b90b696c7032583
Christine <pywongak@connect.ust.hk>
2017-02-14 03:00:47 +0800

    Forgot Password.java updated

--- app/src/main/java/com/csefyp2016/gib3/ustsocialapp/ForgotPassword.java
+++ app/src/main/java/com/csefyp2016/gib3/ustsocialapp/ForgotPassword.java
@@ -8,6 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class ForgotPassword extends AppCompatActivity {
+    private static final String URL = "";
+    private String username;
+    private String email;
+
+    EditText usernameInput;
+    EditText emailInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,27 @@ public class ForgotPassword extends AppCompatActivity {
         setContentView(R.layout.activity_forgot_password);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        usernameInput = (EditText) findViewById(R.id.i_forgotPassword_username);
+        emailInput = (EditText) findViewById(R.id.i_forgotPassword_email);
+
+        Button submitButton = (Button) findViewById(R.id.b_forgotPassword_submit);
+        submitButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = usernameInput.getText().toString();
+                email = emailInput.getText().toString();
+
+                if (true) {
+                    startActivity(new Intent(ForgotPassword.this, ConfirmResetPassword.class));
+                }
+                else {
+                    TextView warning = (TextView) findViewById(R.id.view_forgotPassword_warning);
+                    warning.setText("No record for your username or email. Please use the email during the registration and try again.");
+                    warning.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
     }
 
