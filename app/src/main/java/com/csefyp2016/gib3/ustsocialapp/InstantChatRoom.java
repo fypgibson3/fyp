fcde8f93579edc8957d51e97d5721b2dff54e757
Christine <pywongak@connect.ust.hk>
2017-04-10 11:04:15 +0800

    first draft of instant chat room

--- app/src/main/java/com/csefyp2016/gib3/ustsocialapp/InstantChatRoom.java
+++ app/src/main/java/com/csefyp2016/gib3/ustsocialapp/InstantChatRoom.java
@@ -1,13 +1,47 @@
 package com.csefyp2016.gib3.ustsocialapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class InstantChatRoom extends AppCompatActivity {
+
+    private final static String TAG = "InstantChatRoom";
+    private static final String profilePreference = "ProfilePreference";
+
+    private Socket mSocket;
+    private List<Message> mMessages = new ArrayList<Message>();
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView mMessagesView;
+    private EditText mInputMessageView;
+    private String mUsername = "user name";
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +49,98 @@ public class InstantChatRoom extends AppCompatActivity {
         setContentView(R.layout.activity_instant_chat_room);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mAdapter = new MessageAdapter(this, mMessages);
+        mMessagesView = (RecyclerView) findViewById(R.id.messages);
+        mMessagesView.setLayoutManager(new LinearLayoutManager(this));
+        mMessagesView.setAdapter(mAdapter);
 
+        mInputMessageView = (EditText) findViewById(R.id.message_input);
+        mInputMessageView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int id, KeyEvent event) {
+                if (id == R.id.send || id == EditorInfo.IME_NULL) {
+                    attemptSend();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        ImageButton sendButton = (ImageButton) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptSend();
+            }
+        });
+
+        sharedPreferences = getSharedPreferences(profilePreference, Context.MODE_PRIVATE);
+        mUsername = sharedPreferences.getString("DISPLAYNAME", null);
+
+        MyApplication app = (MyApplication) getApplication();
+        mSocket = app.getSocket();
+        mSocket.on("new message", onNewMessage);
+        mSocket.connect();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSocket.disconnect();
+
+        mSocket.off("new message", onNewMessage);
+    }
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    String username;
+                    String message;
+                    try {
+                        username = data.getString("username");
+                        message = data.getString("message");
+                    } catch (JSONException e) {
+                        Log.e(TAG, e.getMessage());
+                        return;
+                    }
+
+                    addMessage(username, message);
+                }
+            });
+        }
+    };
+
+    private void attemptSend() {
+        if (null == mUsername) return;
+        if (!mSocket.connected()) return;
+
+        String message = mInputMessageView.getText().toString().trim();
+        if (TextUtils.isEmpty(message)) {
+            mInputMessageView.requestFocus();
+            return;
+        }
+
+        mInputMessageView.setText("");
+        addMessage(mUsername, message);
+
+        // perform the sending message attempt.
+        mSocket.emit("new message", message);
+    }
+
+    private void addMessage(String username, String message) {
+        mMessages.add(new Message.Builder(Message.TYPE_MESSAGE)
+                .username(username).message(message).build());
+        mAdapter.notifyItemInserted(mMessages.size() - 1);
+        scrollToBottom();
+    }
+
+    private void scrollToBottom() {
+        mMessagesView.scrollToPosition(mAdapter.getItemCount() - 1);
     }
 
 }
