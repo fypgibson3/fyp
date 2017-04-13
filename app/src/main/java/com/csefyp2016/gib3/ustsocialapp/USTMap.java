97d25efed7cdcae44893b1f9badc2a4399b1c97e
Christine <pywongak@connect.ust.hk>
2017-04-13 14:42:47 +0800

    InstantChatRoom.java 1. Receive chats, detect new comers and leavers, count participants --> finished 2. Read previous 10 chat logs when first join instant chatroom (from database) --> finished
    item_message.xml
    1. Support long messages (auto next line) --> finished
    
    IndividualChat.java
    1. Receive chats --> finished
    2. Save chats --> under development
    
    USTMap.java
    1. Try to detect change on access point connection changes --> developing
    
    AddNewVote.java
    1. Minor addition for vote input id --> testing (optional use, can be deleted if found no use)

--- app/src/main/java/com/csefyp2016/gib3/ustsocialapp/USTMap.java
+++ app/src/main/java/com/csefyp2016/gib3/ustsocialapp/USTMap.java
@@ -1,8 +1,11 @@
 package com.csefyp2016.gib3.ustsocialapp;
 
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +15,17 @@ import android.net.wifi.WifiManager;
 import android.widget.TextView;
 import android.content.Context;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 
 public class USTMap extends Fragment {
+    private static final String URL = "http://ec2-52-221-30-8.ap-southeast-1.compute.amazonaws.com/getMapLocation.php";
+    private RequestQueue requestQueue;
 
     private String SSID;
     private int IP;
@@ -26,10 +34,39 @@ public class USTMap extends Fragment {
     private Button instantChatroom;
     private Button instantChatroom2;
 
+    //  --------------------------------------------------------------- Testing Line  --------------------------------------------------------------- //
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION)) {
+                WifiManager wifiMgr = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+                SSID = wifiInfo.getBSSID();
+                location.setText(SSID);
+            }
+        }
+    };
+    //  --------------------------------------------------------------- Testing Line  --------------------------------------------------------------- //
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        requestQueue = Volley.newRequestQueue(view.getContext());
+
+        location = (TextView) view.findViewById(R.id.view_map_locaion_des);
+        WifiManager wifiMgr = (WifiManager) view.getContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+        SSID = wifiInfo.getBSSID();
+        location.setText(SSID);
+
+        //  --------------------------------------------------------------- Testing Line  --------------------------------------------------------------- //
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
+        view.getContext().registerReceiver(broadcastReceiver, intentFilter);
+        //  --------------------------------------------------------------- Testing Line  --------------------------------------------------------------- //
 
         instantChatroom = (Button) view.findViewById(R.id.b_map_instant_chatroom);
         instantChatroom.setOnClickListener(new Button.OnClickListener() {
@@ -40,6 +77,8 @@ public class USTMap extends Fragment {
                 startActivity(intent);
             }
         });
+
+        //  --------------------------------------------------------------- Testing Line  --------------------------------------------------------------- //
         instantChatroom2 = (Button) view.findViewById(R.id.b_map_instant_chatroom2);
         instantChatroom2.setOnClickListener(new Button.OnClickListener() {
             @Override
@@ -49,11 +88,10 @@ public class USTMap extends Fragment {
                 startActivity(intent);
             }
         });
+        //  --------------------------------------------------------------- Testing Line  --------------------------------------------------------------- //
 
-        location = (TextView) view.findViewById(R.id.view_map_locaion_des);
-        WifiManager wifiMgr = (WifiManager) view.getContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
-        SSID = wifiInfo.getBSSID();
         return view;
     }
+
+
 }
