4671d3f401ae5944669802055f69037628e52c7a
Christine <pywongak@connect.ust.hk>
2017-04-21 19:07:37 +0800

    Request.java 1. Object of an friend request
    RequestAdapter.java
    1. Adapter class for friend requests
    
    FriendRequest.java
    1. Control display and action on friend request responses
    
    SignIn.java
    1. Add shared preference for pending list (Friend  requests sent and waiting for accept)
    2. Add shared preference for friend request list (Friend requests from other user)
    
    ViewProfile.java
    1. Settings for add friend
    
    content_friend_request.xml
    1. change to recycleView

---
+++ app/src/main/java/com/csefyp2016/gib3/ustsocialapp/RequestAdapter.java
@@ -0,0 +1,56 @@
+package com.csefyp2016.gib3.ustsocialapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.ViewHolder> {
+    private List<Request> mRequests;
+
+    public RequestAdapter (Context context, List<Request> requests) {
+        mRequests = requests;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        int layout = R.layout.friend_request_layout;
+
+        View v = LayoutInflater
+                .from(parent.getContext())
+                .inflate(layout, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Request request = mRequests.get(position);
+        holder.setmRequestContent(request.getFriendId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRequests.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView mRequestContent;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mRequestContent = (TextView) itemView.findViewById(R.id.view_friendRequest_title);
+        }
+
+        public void setmRequestContent(String content) {
+            if (null == mRequestContent) return;
+            mRequestContent.setText(content);
+        }
+
+    }
+}
